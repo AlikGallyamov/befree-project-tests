93173ebaa94451072e5d45e38e135f26fe635a67
Alik Gallyamov <alik.gallyamov2202@gmail.com>
2024-03-11 04:07:40 +0500

    merge api methode

--- befree_tests/api/objects_api.py
+++ befree_tests/api/objects_api.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from befree_tests.contorls.utils import get_project_path
 from befree_tests.data.json_body.request_body import auth_body, add_product_body, headers_auth, \
     delete_item_from_cart_body, favorites
-from befree_tests.helpers.api_requests import post_request, get_request, delete_request
+from befree_tests.helpers.api_requests import request_api
 from befree_tests.helpers.find_element_to_json import search_json, search_json_value
 
 
@@ -23,12 +23,13 @@ class ObjectsApi:
 
     @allure.step('Запрашиваем данные авторизации')
     def get_auth_response(self):
-        method = '/rest/V3/login'
+        endpoint = '/rest/V3/login'
+        methode = 'post'
         data = auth_body
         data['email'] = self.email
         data['password'] = self.password
 
-        response = post_request(self.back_url, method, json=data)
+        response = request_api(self.back_url, endpoint, methode, json=data)
         return response
 
     @allure.step('Добавляем товар в корзину по АПИ')
@@ -37,42 +38,46 @@ class ObjectsApi:
         data['product'] = product_variation_id_in_card
         headers = headers_auth
         headers['Authorization'] = 'Bearer ' + token
-        method = '/rest/V3/cart'
-        response = post_request(self.back_url, method, headers=headers, json=data)
+        endpoint = '/rest/V3/cart'
+        methode = 'post'
+        response = request_api(self.back_url, endpoint, methode, headers=headers, json=data)
         return response
 
     @allure.step('Запрашиваем информацию о корзине')
     def get_cart_info(self, token):
-        methode = '/rest/V3/customer?full=1&with=level'
+        endpoint = '/rest/V3/customer?full=1&with=level'
+        methode = 'get'
         headers = headers_auth
         headers['Authorization'] = 'Bearer ' + token
-        response = get_request(self.back_url, methode, headers=headers)
+        response = request_api(self.back_url, endpoint, methode, headers=headers)
 
         return response
 
     @allure.step('Запрашиваем информацию по товару')
     def get_items_info_from_cart(self, token, cart_id):
-        methode = '/rest/V3/cart/'
+        endpoint = '/rest/V3/cart/'
+        methode = 'get'
         headers = headers_auth
         headers['Authorization'] = 'Bearer ' + token
 
-        methode = methode + cart_id
+        endpoint = endpoint + cart_id
 
-        response = get_request(self.back_url, methode, headers=headers)
+        response = request_api(self.back_url, endpoint, methode, headers=headers)
         return response
 
     @allure.step('Удаляем товар из корзины по АПИ')
     def remove_item_from_cart(self, token, product_variation_id_in_catalog):
         cart_id = self.get_cart_info(token).json()['cartId']
         items_info = self.get_items_info_from_cart(token, cart_id).json()
-        methode = '/rest/V3/cart/'
+        endpoint = '/rest/V3/cart/' + cart_id
+        methode = 'delete'
         item_id = search_json(items_info, 'product_variation_id', product_variation_id_in_catalog)
         data = delete_item_from_cart_body
         data['itemIds'].append(item_id)
 
         headers = headers_auth
         headers['Authorization'] = 'Bearer ' + token
-        response = delete_request(self.back_url, methode + cart_id, headers=headers, json=data)
+        response = request_api(self.back_url, endpoint, methode, headers=headers, json=data)
 
         return response
 
@@ -83,39 +88,43 @@ class ObjectsApi:
 
     @allure.step('Генерируем UUID')
     def generate_favorite_uuid(self, token):
-        methode = '/public/v2/favorites'
+        endpoint = '/public/v2/favorites'
+        methode = 'get'
         headers = headers_auth
         headers['Authorization'] = 'Bearer ' + token
-        response = get_request(self.catalog_url, methode, headers=headers)
+        response = request_api(self.catalog_url, endpoint, methode, headers=headers)
         return response
 
     @allure.step('Удаляем товар из избранного по АПИ')
     def delete_item_favorite(self, token, uuid, article, color_code):
-        methode = '/public/v2/favorites/' + uuid
+        endpoint = '/public/v2/favorites/' + uuid
+        methode = 'delete'
         headers = headers_auth
         headers['Authorization'] = 'Bearer ' + token
         data = favorites
         data['article'] = article
         data['color_code'] = color_code
-        delete_request(self.catalog_url, methode, headers=headers, json=data)
+        request_api(self.catalog_url, endpoint, methode, headers=headers, json=data)
 
     @allure.step('Запрашиваем содержимое избранного')
     def get_favorite_items(self, token, uuid):
-        methode = '/public/v2/favorites/' + uuid
+        endpoint = '/public/v2/favorites/' + uuid
+        methode = 'get'
         headers = headers_auth
         headers['Authorization'] = 'Bearer ' + token
-        response = get_request(self.catalog_url, methode, headers=headers)
+        response = request_api(self.catalog_url, endpoint, methode, headers=headers)
         return response
 
     @allure.step('Добавляем товар в избранное по АПИ')
     def add_item_to_favorite(self, token, uuid, article, color_code):
-        methode = '/public/v2/favorites/' + uuid
+        endpoint = '/public/v2/favorites/' + uuid
+        methode = 'post'
         headers = headers_auth
         headers['Authorization'] = 'Bearer ' + token
         data = favorites
         data['article'] = article
         data['color_code'] = color_code
-        return post_request(self.catalog_url, methode, headers=headers, json=data)
+        return request_api(self.catalog_url, endpoint, methode, headers=headers, json=data)
 
     def item_in_favorite(self, data, article):
         return search_json_value(data, 'article', article)
--- befree_tests/helpers/api_requests.py
+++ befree_tests/helpers/api_requests.py
@@ -7,34 +7,16 @@ from allure_commons._allure import step
 from allure_commons.types import AttachmentType
 
 
-def post_request(url, methode, **kwargs):
-    response = requests.post(url + methode, **kwargs)
-    if methode != '/rest/V3/login':
-        with step(f"POST{methode}"):
+def request_api(url, endpoint, methode, **kwargs):
+    if methode == "get":
+        response = requests.get(url + endpoint, **kwargs)
+    if methode == "post":
+        response = requests.post(url + endpoint, **kwargs)
+    if methode == "delete":
+        response = requests.delete(url + endpoint, **kwargs)
+    if endpoint != '/rest/V3/login':
+        with step(f"POST{endpoint}"):
             curl = curlify.to_curl(response.request)
             logging.info(curl)
             allure.attach(body=curl, name="curl", attachment_type=AttachmentType.TEXT, extension="txt")
-    return response
-
-
-def get_request(url, methode, **kwargs):
-    response = requests.get(url + methode, **kwargs)
-    if methode != '/rest/V3/login':
-        with step(f"GET{methode}"):
-            curl = curlify.to_curl(response.request)
-            logging.info(curl)
-            allure.attach(body=curl, name="curl", attachment_type=AttachmentType.TEXT, extension="txt")
-    return response
-
-
-def delete_request(url, methode, **kwargs):
-    response = requests.delete(url + methode, **kwargs)
-    if methode != '/rest/V3/login':
-        with step(f"DELETE{methode}"):
-            curl = curlify.to_curl(response.request)
-            status = response.status_code
-            logging.info(curl)
-            logging.info(status)
-            allure.attach(body=curl, name="curl", attachment_type=AttachmentType.TEXT, extension="txt")
-
     return response
