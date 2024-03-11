2ac6ce6f10574b7e18759afdc51d61b396b129f8
Alik Gallyamov <alik.gallyamov2202@gmail.com>
2024-03-11 23:20:14 +0500

    added loging new

--- befree_tests/helpers/api_requests.py
+++ befree_tests/helpers/api_requests.py
@@ -1,7 +1,7 @@
 import allure
 import requests
 import logging
-
+import json
 import curlify
 from allure_commons._allure import step
 from allure_commons.types import AttachmentType
@@ -15,12 +15,19 @@ def request_api(url, endpoint, methode, **kwargs):
     if methode == "delete":
         response = requests.delete(url + endpoint, **kwargs)
     if endpoint != '/rest/V3/login':
-        with step(f"{methode}{endpoint}"):
-            curl = curlify.to_curl(response.request)
-            logging.info(curlify.to_curl(response.request))
+        # with step(f"{methode}{endpoint}"):
+        #     curl = curlify.to_curl(response.request)
+        #     logging.info(curlify.to_curl(response.request))
+        #     logging.info(response.request.url)
+        #     logging.info(response.status_code)
+        #     logging.info(response.text)
+        #     allure.attach(body=curl, name="curl", attachment_type=AttachmentType.TEXT, extension="txt")
+        with allure.step('API Request'):
+            allure.attach(body=response.request.method + ' ' + response.request.url, name='Request',
+                          attachment_type=AttachmentType.TEXT, extension='txt')
+            allure.attach(body=json.dumps(response.json(), indent=4, ensure_ascii=True), name=''"Response",
+                          attachment_type=AttachmentType.JSON, extension='json')
             logging.info(response.request.url)
             logging.info(response.status_code)
             logging.info(response.text)
-            allure.attach(body=curl, name="curl", attachment_type=AttachmentType.TEXT, extension="txt")
-
     return response
